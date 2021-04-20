b26f6b2f44aaec29a8dbf94b523fda8e3630e21d
olirek1 <aloksaumyawork@gmail.com>
2021-04-20 22:43:08 +0530

    “final”

--- server/controller/controller.js
+++ server/controller/controller.js
@@ -24,3 +24,28 @@ exports.create = (req,res)=>{
             });
         });
 }
+
+// find recent searches
+exports.recent = (req,res)=>{
+    Citydb.find().sort({_id:-1}).limit(10)
+    .then(city=>{
+        res.status(200).send(city)
+    })
+    .catch(err =>{
+        res.status(500).send({ message : err.message || "Error occured while retrieving the data"});
+    })
+}
+
+exports.mostsearch = (req,res)=>{
+    Citydb.aggregate([
+        {"$group" : {_id:"$cityname", "count": {$sum:1}}},
+        {"$sort" : { count:-1}}
+        // {"$group" : {_id:null,city "count": {$sum:"$cityname"}}},
+    ])
+    .then(city=>{
+        res.status(200).send(city)
+    })
+    .catch(err =>{
+        res.status(500).send({ message : err.message || "Error occured while retrieving the data"});
+    })
+}
--- server/routes/router.js
+++ server/routes/router.js
@@ -5,6 +5,8 @@ const controller = require('../controller/controller');
 
 
 route.post('/api/cities',controller.create);
+route.get('/api/recent', controller.recent);
+route.get('/api/mostsearch', controller.mostsearch);
 
 
 module.exports = route;
